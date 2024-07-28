c8c742dbc7cceca44e24bdfad9860bdf75704a29
githubHZB <156395304+githubHZB@users.noreply.github.com>
2024-07-28 15:45:09 +0800

    提交初识Java代码IDEA书写

---
+++ Java_7_28/src/hello_world.java
@@ -0,0 +1,9 @@
+/**
+ * @author: HZB
+ * @create: 2024-07-28 下午3:32
+ */
+public class hello_world {
+    public static  void main (String[] args) {
+        System.out.println("hello world 你好世界~~");
+    }
+}
