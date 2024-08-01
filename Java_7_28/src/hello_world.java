a7b89e53a36727e505a3c5fc6898aa06ae9316e3
githubHZB <156395304+githubHZB@users.noreply.github.com>
2024-08-01 17:26:37 +0800

    提交初识Java代码IDEA书写

--- Java_7_28/src/hello_world.java
+++ Java_7_28/src/hello_world.java
@@ -1,9 +1,18 @@
 /**
- * @author: HZB
+ * @author: LenovoHZB
  * @create: 2024-07-28 下午3:32
  */
+//这是java的快速入门，演示java的开发步骤
+//对代码的相关说明
+//1. public class hello 表示Hello是一个类，是一个public公有的类
+//2. Hello{} 表示一个
+//3. public static void main(String[] args) 表示一个主方法,即我们程序的入口
+//4. main() {} 表示方法的开始和结束
+//5. System.out.println("hello,world~"); 表示输出"hello,world~"到屏幕//6. ;表示语句结束
 public class hello_world {
-    public static  void main (String[] args) {
-        System.out.println("hello world 你好世界~~");
+
+    //编写一个main方法
+    public static void main(String[] args){
+        System.out.println("hello,world~");
     }
 }
