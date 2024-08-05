f8bcd3a87d180e676db19e91aaacf219cacddae4
githubHZB <156395304+githubHZB@users.noreply.github.com>
2024-08-05 21:40:27 +0800

    提交转义字符课堂练习代码

---
+++ Java_8_5/src/转义字符课堂练习.java
@@ -0,0 +1,12 @@
+/**
+ * @author: LenovoHZB
+ * @create: 2024-08-05 下午9:15
+ */
+public class 转义字符课堂练习 {
+
+    //编写一个main方法
+    public static void main(String[] args) {
+        //完成代码
+        System.out.println("书名\t作者\t价格\t销量\n三国\t罗贯中\t120\t1000");
+    }
+}
