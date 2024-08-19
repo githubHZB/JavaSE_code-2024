f3c05868ac0962fb2b3a69a9ed1da39386784daa
githubHZB <156395304+githubHZB@users.noreply.github.com>
2024-08-19 19:24:35 +0800

    编写hello world程序[Homework01.java]

---
+++ Java_8_19/src/Homework01.java
@@ -0,0 +1,12 @@
+/**
+ * @author: LenovoHZB
+ * @create: 2024-08-19 下午7:17
+ */
+public class Homework01 {
+
+    // 编写一个main方法
+    public static void main(String[] args){
+        //编写hello, world程序
+        System.out.println("hello world");
+    }
+}
